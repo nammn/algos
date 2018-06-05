27563f78f8c2cfe319d573cf4006f24d92552418
nam <nam.nguyen@de.ibm.com>
2018-06-05 15:27:46 +0200

    added new tasks

--- trees/Tree.py
+++ trees/Tree.py
@@ -76,9 +76,18 @@ class Node:
             if node.right is not None:
                 queue.en_queue(node.right)
 
-    # rebalance the tree, such that a search and insertion time of log*n to be the avg. case
-    def balanceTree(self):
+    # rebalance the tree, such that a search and insertion time of log*n to be the avg. case of skewed tree
+    def balance_tree(self):
         print(1)
+
+    def sum(self, node):
+        if node is None:
+            return 0
+        else:
+            left = self.sum(node.left)
+            right = self.sum(node.right)
+            mysum = node.value + left + right
+            return mysum
 
     def max(self):
         print(1)
@@ -98,12 +107,16 @@ class Node:
 
 root = Node(42)
 inserts = [29, 27, 57, 71, 30, 97, 41, 78, 3, 60, 28, 100, 50]
+sum = root.value
 for i in inserts:
     n1 = Node(i)
     root.add_node(n1)
+    sum += i
 print('inserted: ', inserts)
+print('sum: ', sum)
 
-print(root.search(70))
+print('searching for 70: ', root.search(70))
+print('searching for 50: ', root.search(50))
 print('\n preOrder')
 root.traverse_pre_order()
 print('\n inOrder')
@@ -112,3 +125,5 @@ print('\n postOrder')
 root.traverse_post_order()
 print('\n bfs')
 root.traverse_bfs()
+print('\n sum')
+print(root.sum(root))
